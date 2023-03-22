6b685cd732cdb7b89b5dbe1ee96d4f9f2ea70738
Asif <kallamaduguasif786@gmail.com>
2023-03-22 12:00:56 +0530

    commting changes in file0.js

--- Git_Intro/file0.js
+++ Git_Intro/file0.js
@@ -2,3 +2,6 @@ function greet(name) {
   console.log("hello " + name);
 }
 greet("Asif");
+// we use git diff to check the differences in files
+// only for the files which are not added or unstaged
+// once stagged won't show differencea
