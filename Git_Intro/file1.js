17ae86c3a15b23913855b9224af0ec0b3ac3b6bb
Asif <kallamaduguasif786@gmail.com>
2023-03-22 11:38:31 +0530

    modified file1.js and commited in git

--- Git_Intro/file1.js
+++ Git_Intro/file1.js
@@ -1,1 +1,7 @@
 console.log("hey we are learning git");
+// we use git add {file name} to add files to git
+// we use git commit to commit the added files to git
+//if we use only git commit vim editor will open and we need to add a
+//commit message
+// along with git commit we can provide message using -m
+//ex: git commit -m "commit message"
